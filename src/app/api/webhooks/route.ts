703b87f5f879ea41d67a08896984e1e13a334d91
Bello Olamilekan <belloolamilekan661@gmail.com>
2024-07-01 21:36:56 +0100

    final deployment

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
       });
 
       await resend.emails.send({
-        from: "CaseCobra <hello@belloolamilekan.com>",
+        from: "CaseCobra <belloolamilekan661@gmail.com>",
         to: [event.data.object.customer_details.email],
         subject: "Thanks for your order!",
         react: OrderReceivedEmail({
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
-import { cn } from "@/lib/utils";
+import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
@@ -9,20 +7,32 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 const Navbar = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
+
   const isAdmin = user?.email === process.env.ADMIN_EMAIL;
+
   return (
-    <nav className=" sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+    <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
-        <div className=" flex h-14 items-center justify-between border-b border-zinc-200">
-          <Link href={"/"} className="flex z-40 font-semibold">
-            case<span className=" text-green-600">cobra</span>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+          <Link href="/" className="flex z-40 font-semibold">
+            case<span className="text-green-600">cobra</span>
           </Link>
-          <div className=" h-full flex items-center space-x-4">
+
+          <div className="h-full flex items-center space-x-4">
             {user ? (
               <>
+                <Link
+                  href="/api/auth/logout"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign out
+                </Link>
                 {isAdmin ? (
                   <Link
-                    href={"/dashboard"}
+                    href="/dashboard"
                     className={buttonVariants({
                       size: "sm",
                       variant: "ghost",
@@ -32,29 +42,20 @@ const Navbar = async () => {
                   </Link>
                 ) : null}
                 <Link
-                  href={"/configure/upload"}
+                  href="/configure/upload"
                   className={buttonVariants({
                     size: "sm",
                     className: "hidden sm:flex items-center gap-1",
                   })}
                 >
                   Create case
-                  <ArrowRight className=" ml-1.5 h-5 w-5" />
-                </Link>
-                <Link
-                  href={"/api/auth/logout"}
-                  className={buttonVariants({
-                    size: "sm",
-                    variant: "ghost",
-                  })}
-                >
-                  Sign out
+                  <ArrowRight className="ml-1.5 h-5 w-5" />
                 </Link>
               </>
             ) : (
               <>
                 <Link
-                  href={"/api/auth/register"}
+                  href="/api/auth/register"
                   className={buttonVariants({
                     size: "sm",
                     variant: "ghost",
@@ -62,8 +63,9 @@ const Navbar = async () => {
                 >
                   Sign up
                 </Link>
+
                 <Link
-                  href={"/api/auth/login"}
+                  href="/api/auth/login"
                   className={buttonVariants({
                     size: "sm",
                     variant: "ghost",
@@ -71,16 +73,18 @@ const Navbar = async () => {
                 >
                   Login
                 </Link>
-                <div className=" h-8 w-px bg-zinc-200 hidden sm:block"></div>
+
+                <div className="h-8 w-px bg-zinc-200 hidden sm:block" />
+
                 <Link
-                  href={"/configure/upload"}
+                  href="/configure/upload"
                   className={buttonVariants({
                     size: "sm",
                     className: "hidden sm:flex items-center gap-1",
                   })}
                 >
                   Create case
-                  <ArrowRight className=" ml-1.5 h-5 w-5" />
+                  <ArrowRight className="ml-1.5 h-5 w-5" />
                 </Link>
               </>
             )}
